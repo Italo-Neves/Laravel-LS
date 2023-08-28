e7663d86ad31d1bd9d47bdbca6786a760fc93618
Italo Neves <italovinicius@gmail.com>
2023-08-28 15:44:58 -0300

    criando novas migrates

---
+++ primeiro_exemplo/database/migrations/2023_08_28_151415_create_enderecos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEnderecosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enderecos', function (Blueprint $table) {
+            $table->id();
+            $table->string('rua', 100);
+            $table->integer('numero');
+            $table->string('complemento', 10);
+            $table->string('bairro', 100);
+
+            $table->foreignId('imovel_id')->constrained('imoveis')->onDelete('cascade'); //Neste caso como como o nome da migrate gerada foi altareada precisamo inserir o nome da tabela na constrained
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enderecos');
+    }
+}
